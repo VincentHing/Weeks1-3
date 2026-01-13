0bdee0cb8c31a9106e5aed2880151bef8404dfbb
Vincent Hingley <hingley@shernet.sheridancollege.ca>
2026-01-13 11:45:45 -0500

    finished code gym

--- Assets/scripts/gym scripts/teleport.cs
+++ Assets/scripts/gym scripts/teleport.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class teleport : MonoBehaviour
 {
     public float t;
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +13,18 @@ public class teleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 newPos = transform.position;
+        
         t += Time.deltaTime;
         if (t > 3) {
+            Vector2 screenPos = new Vector2(Random.Range(0,Screen.width), Random.Range(0,Screen.height));
+           newPos = Camera.main.ScreenToWorldPoint(screenPos);
             t = 0;
         }
-        Vector2 screenPos = 
+        transform.position = newPos;
+       
+        
+
 
         
     }
